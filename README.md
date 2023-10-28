b91a637ed4e188f21451b2c8ec4be9408dcf6ff0
GiampieroFC <giampieroferminicastillo@gmail.com>
2023-10-29 02:41:32 +0300

    package.json and tsconfig.json configured correctly

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Crypt-it
+# Crypt-it
 
 ## Description
 So simple program to encrypt any file using the module 'crypto' of node.js.
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,58 @@
 import { createCipheriv, createDecipheriv } from 'node:crypto';
+import { toNBytes } from './config/plugins/to-n-bytes.js';
+import inquirer from 'inquirer';
+
 
 const algorithm = 'aes-256-cbc';
 const key = 'soy la key';
-const ini = 65;
-const password = Buffer.alloc(32, key, 'binary');
-const iv = Buffer.alloc(16, ini, 'binary');
+const ini = key.length;
+
+
+(async () => {
+
+    const opt = await inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'files',
+            message: 'Choose file to encrypt',
+            choices: [
+                {
+                    name: 'file 1',
+                    value: 'path del file 1',
+                    short: 'short 1',
+                },
+                {
+                    name: 'file 2',
+                    value: 'path del file 2',
+                    short: 'short 2',
+                },
+                {
+                    name: 'file 3',
+                    value: 'path del file 3',
+                    short: 'short 3',
+                },
+                {
+                    name: 'file 4',
+                    value: 'path del file 4',
+                    short: 'short 4',
+                },
+                {
+                    name: 'file 5',
+                    value: 'path del file 5',
+                    short: 'short 5',
+                },
+
+            ]
+        }
+    ]);
+
+    console.log(opt)
+})()
+
+
+const password = toNBytes(32, key);
+const iv = toNBytes(16, ini);
+
 const cipher = createCipheriv(algorithm, password, iv);
 
 let encrypted = cipher.update('laborum Aut sequi ducimus et. Assumenda nesciunt odit autem et asperiores mollitia reprehenderit totam. Nulla sapiente inventore velit sint molestiae. Quod sed quis.', 'utf8', 'binary');
